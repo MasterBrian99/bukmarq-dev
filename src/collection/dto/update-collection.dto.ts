f1aece5b6cd4dd4095d8beac929372420993f2cb
Pasindu P Konghawaththa <pasindudixon25@gmail.com>
2023-02-16 19:35:49 +0530

    remove old files

--- src/collection/dto/update-collection.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty, PartialType } from '@nestjs/swagger';
-import { CreateCollectionDto } from './create-collection.dto';
-
-export class UpdateCollectionDto extends PartialType(CreateCollectionDto) {
-  @ApiProperty()
-  collectionId: number;
-  @ApiProperty()
-  unified: string;
-}
--- src/common/error-messages.ts
+++
@@ -1,6 +0,0 @@
-const ERROR_MESSAGES = {
-  INTERNAL_SERVER_ERROR: 'Something went wrong. Please try again later.',
-  USER_ALREADY_EXIST_ERROR: 'User Already Exist',
-};
-
-export default ERROR_MESSAGES;
